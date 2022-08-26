a670ff9acd252472b60a770ef0e348dd15c8969e
siva <siva@Siva>
2022-08-26 19:58:56 +0530

    added external jars

--- EclipseToGithubPushTesting/src/testingEclipseToGithubPush/GithubTest2.java
+++ EclipseToGithubPushTesting/src/testingEclipseToGithubPush/GithubTest2.java
@@ -1,10 +1,16 @@
 package testingEclipseToGithubPush;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class GithubTest2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.setProperty("webdriver.chrome.driver", "C:\\selenium\\WebDrivers\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.google.co.in/");
 	}
 
 }
